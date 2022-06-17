b75124ee48999211dac197210c9813f46180dcc3
Sergieo <sergieol23@gmail.com>
2022-06-17 01:48:58 -0500

    added offline functionality

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const webpack = require('webpack');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
-  .BundleAnalyzerPlugin;
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 // const SWPrecacheWebpackPlugin = require("sw-precache-webpack-plugin");
 // const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require('path');
@@ -63,7 +62,11 @@ const config = {
     //   }]
     // })
   ],
-  mode: 'development'
+  mode: 'development',
+
+  devServer: {
+    static: './'
+  }
 };
 
 module.exports = config;
